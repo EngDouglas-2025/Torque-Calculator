a325d7e45c352b31b7a636dbe2254c06d9ce5adc
EngDouglas-2025 <engmichaeldmagalhaes@gmail.com>
2025-04-23 13:23:44 -0300

    Add files via upload

---
+++ torque-calculator/torque-calculator/README.md
@@ -0,0 +1,3 @@
+# Torque Calculator
+
+Calculadora de torque para parafusos. Criada com React + Tailwind.
---
+++ torque-calculator/torque-calculator/src/App.js
@@ -0,0 +1,12 @@
+
+import TorqueCalculator from "./components/TorqueCalculator";
+
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-50 p-4">
+      <TorqueCalculator />
+    </div>
+  );
+}
+
+export default App;
